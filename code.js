b33e3a84c19f258a4b0bb9ca65c9141c639df331
Andrew Papacostas <a_papacostas@yahoo.com>
2016-05-21 04:48:38 +0300

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(icecream) {
+  return "I love " + icecream;
+}
+
+function shouting(scream) {
+  return scream.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
